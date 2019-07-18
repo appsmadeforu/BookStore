a02f9e0ac7de58637be9bdc00eb95572cc502a2e
Sofiya Rao <34954341+appsmadeforu@users.noreply.github.com>
2019-07-18 18:06:40 +0530

    static file added

--- store/views.py
+++ store/views.py
@@ -12,10 +12,11 @@ def store(request):
     count = Book.objects.all().count()
     context = {
         'count': count,
+        'page': 'welcome to mystery books!',
     }
     request.session['location'] = "unknown"
 
     if request.user.is_authenticated:
         request.session['location'] = "Pune"
 
-    return render(request, "store.html", context)
+    return render(request, "base.html", context)
