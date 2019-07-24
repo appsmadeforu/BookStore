cb59786c63b2103980dcf7b93abab2779a869483
Sofiya Rao <34954341+appsmadeforu@users.noreply.github.com>
2019-07-24 19:41:55 +0530

    Ecommerce 10.46 1st vid

--- store/models.py
+++ store/models.py
@@ -1,12 +1,25 @@
 from django.utils import timezone
 
 from django.db import models
+from django.contrib.auth.models import User
 
+class Author(models.Model):
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return '%s, %s' % (self.last_name, self.first_name)
 
 class Book(models.Model):
     title = models.CharField(max_length=200)
-    author = models.CharField(max_length=200)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE,)
     description = models.TextField()
     publish_date = models.DateField(default=timezone.now)
     price = models.DecimalField(decimal_places=2, max_digits=8)
     stock = models.IntegerField(default=0)
+
+class Review(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE,)
+    user = models.ForeignKey(User, on_delete=models.CASCADE,)
+    publish_date = models.DateField(default=timezone.now)
+    text = models.TextField()
